c64bc964929fc5f9dfa8cb094c6bc0ce9e4e3f7f
DeVon DuPree <devondupree@Air-DeVonny-Pooh.local>
2022-12-25 17:19:30 -0800

    links

--- ddupree-portfolio/src/components/NavBar.js
+++ ddupree-portfolio/src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/devon-dupree-973471168/"><img src={navIcon1} alt="" /></a>
                 <a href="/"><img src={navIcon2} alt="" /></a>
-                <a href="/"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.instagram.com/devon_dupree01/?next=%2F"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
